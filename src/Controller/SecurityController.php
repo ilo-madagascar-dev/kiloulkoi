4b18c1403de91f79758724eb98873741d272e34d
yowan william <yowanwilliam@gmail.com>
2020-06-02 10:44:39 +0300

    On a créer une form clientType pour client

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use App\Form\ClientType;
 use App\Form\RegistrationType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
@@ -16,13 +17,13 @@ class SecurityController extends AbstractController
      /**
      * @Route("/inscriptionUtilisateur", name="security_registrationlog")
      */
-    public function registrationPrestataire(Request $request, EntityManagerInterface $em,
+    public function registrationClient(Request $request, EntityManagerInterface $em,
     UserPasswordEncoderInterface $encoder)
  
     {
       $user = new User();
 
-      $form = $this->createForm(RegistrationType::class, $user);
+      $form = $this->createForm(ClientType::class, $user);
 
       $form->handleRequest($request);
 
@@ -30,6 +31,12 @@ class SecurityController extends AbstractController
         $hash = $encoder->encodePassword($user, $user->getPassword());
         $user->setPassword($hash);
         $user->setFonction('ROLE_CLIENT');
+        $user->setCp('');
+        $user->setVille('');
+        $user->setTelephone('');
+        $user->setNom('');
+        $user->setRue('');
+        $user->setPrenom('');
           // l'objet $em sera affecté automatiquement grâce à l'injection des dépednaces 
           $em->persist($user);
           $em->flush();
@@ -41,7 +48,7 @@ class SecurityController extends AbstractController
      /**
      * @Route("/inscriptionpresta", name="security_registrationpresta")
      */
-    public function registrationclient(Request $request, EntityManagerInterface $em,
+    public function registrationPrestataire(Request $request, EntityManagerInterface $em,
     UserPasswordEncoderInterface $encoder)
  
     {
