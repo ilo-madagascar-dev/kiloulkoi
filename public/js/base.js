f47c7b3e78cf8df5fc8a39cf21f3209cdd897bf1
RAZANANIRIANA Mendrika Nomenjanahary <rzmendrika@gmail.com>
2020-11-27 14:17:52 +0300

    Séparation des js

---
+++ public/js/base.js
@@ -0,0 +1,22 @@
+$(document).ready( function () 
+{
+	var words = $('.categorie-parent').map( function(i, e) { return $(e).text().trim() });
+	console.log( words );
+	var index = 0;
+	
+	var show = function() 
+	{
+		setTimeout(function () {
+			$('.slogan-animation').fadeOut(1000, function() 
+			{
+				$('.slogan-animation').text(words[index]);
+				index++;
+				if( !words[index] )
+				index = 0;
+				
+				$('.slogan-animation').fadeIn(1500, show);
+			});
+		}, 1000)
+	}
+	show();
+})
