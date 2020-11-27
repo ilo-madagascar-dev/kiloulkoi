d2fcd181b560fce641566de6b61c39c7ba4241e8
RAZANANIRIANA Mendrika Nomenjanahary <rzmendrika@gmail.com>
2020-11-27 16:27:52 +0300

    Separation js v2

---
+++ public/js/accueil.js
@@ -0,0 +1,141 @@
+
+$(document).ready( function () 
+{
+	$('#datepicker').datepicker({
+		format: 'dd/mm/yyyy',
+		uiLibrary: 'bootstrap4',
+		altField: "#datepicker",
+		closeText: 'Fermer',
+		prevText: 'Précédent',
+		nextText: 'Suivant',
+		currentText: 'Aujourd\'hui',
+		monthNames: ['Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre', 'Décembre'],
+		monthNamesShort: ['Janv.', 'Févr.', 'Mars', 'Avril', 'Mai', 'Juin', 'Juil.', 'Août', 'Sept.', 'Oct.', 'Nov.', 'Déc.'],
+		dayNames: ['Dimanche', 'Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi'],
+		dayNamesShort: ['Dim.', 'Lun.', 'Mar.', 'Mer.', 'Jeu.', 'Ven.', 'Sam.'],
+		dayNamesMin: ['Di', 'Lu', 'Ma', 'Mer', 'Je', 'Ve', 'Sa'],
+		weekHeader: 'Sem.',
+		firstDay: 1
+	});
+	
+	$( "#slider-range" ).slider({
+		range: true,
+		min: 0,
+		max: 1000,
+		values: [ 0, 300 ],
+		slide: function( event, ui ) 
+		{
+			$( "#amount" ).val( ui.values[ 0 ] + "€ - " + ui.values[ 1 ] + "€" );
+		}
+	});
+	$( "#amount" ).val( $( "#slider-range" ).slider( "values", 0 ) + "€ - " + $( "#slider-range" ).slider( "values", 1 ) + "€" );
+	
+	$('#input-categorie').val(null);
+	$('#input-show-categorie').click( function(e)
+	{
+		$(this).toggleClass('not-show-all');
+		$('.categorie-container').toggleClass('d-none');
+	})
+
+	$('.categorie-enfant').click( function () 
+	{
+		selectCategorie( $(this) );
+	})
+
+	$('.categorie-parent').click( function () 
+	{
+		if( $(this).attr('data-class') == 'Service' || $(this).attr('data-class') == 'Divers' )
+		{
+			$('#prix-unite option[value=1]').removeClass('d-none');
+			selectCategorie( $(this) );
+		}
+		else
+		{
+			$('#prix-unite option[value=1]').addClass('d-none');
+			$('#prix-unite').val('2');
+		}
+	})
+	
+	$(document).on('click', '#input-show-categorie span i', function (event) 
+	{
+		try {
+			var values = JSON.parse( $('#input-categorie').val() );
+		} catch (e) {
+			var values = [];
+		}
+
+		if( values.length <= 1 )
+		{
+			$('.categorie-placeholder').removeClass('d-none');
+		}
+
+		var val = $(this).parent().attr('data');
+		values  = values.filter( function(ele){ 
+			return ele != val;
+		});
+		values = JSON.stringify( values );
+		$('#input-categorie').val( values );
+		$(this).parent().remove();
+	})
+
+	var selectCategorie = function (categorie)
+	{
+		try {
+			var values = JSON.parse( $('#input-categorie').val() );
+		} catch (e) {
+			var values = [];
+		}
+
+		$('.categorie-placeholder').addClass('d-none');
+		var val = categorie.attr('data');
+		if( !values.includes(val) )
+		{
+			values.push(val);
+			values = JSON.stringify( values );
+
+			var selected = `
+				<span data="${ categorie.attr('data') }">
+					<i>x</i>
+					<em>${ categorie.text().trim() }</em>
+				</span>
+			`;
+			$('#input-show-categorie').append( selected );
+			$('#input-categorie').val( values );
+		}
+	}
+
+	$('#city').select2({
+		language: 'fr',
+		placeholder: 'Séléctionner une ville',
+		templateSelection: function (params) {
+			return params.text;
+		},
+		ajax: {
+			url: function (params) {
+				return "https://datanova.legroupe.laposte.fr/api/records/1.0/search/?dataset=laposte_hexasmal&facet=nom_de_la_commune&facet=code_postal&q=" + params.term;
+			},
+			processResults: function (data) {
+				// Transforms the top-level key of the response object from 'items' to 'results'
+				var response = data.records;
+				var options  = $.map(response, function (obj) {
+					return {
+						text: obj.fields.code_postal + ' - ' + obj.fields.nom_de_la_commune,
+						id:  obj.fields.code_postal + ' - ' + obj.fields.nom_de_la_commune,
+					};
+				});
+
+				return { results: options };
+			}
+		}
+	});
+
+	$(document).click( function(event)
+	{
+		//check if the clicked area is dropDown or not
+		if ( $(".categorie-container *").has(event.target).length === 0 && $("#input-show-categorie *").has(event.target).length === 0 && "input-show-categorie" !== $(event.target).attr('id') ) 
+		{
+			$(".categorie-container").addClass("d-none");
+			$("#input-show-categorie").addClass("not-show-all");
+		}
+    });
+})
--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -9,16 +9,20 @@ use App\Repository\CategoriesRepository;
 use App\Repository\TypeLocationRepository;
 use App\Service\PaginationService;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
+use Symfony\Component\Serializer\Normalizer\AbstractObjectNormalizer;
+use Symfony\Component\Serializer\SerializerInterface;
 
 class AccueilController extends AbstractController
 {
     /**
      * @Route("/", name="accueil")
      */
-    public function index(Request $request, AnnoncesRepository $repAnnonce, CategoriesRepository $repCategorie, TypeLocationRepository $ReptypeLocation, PaginationService $paginator)
+    public function index(Request $request, AnnoncesRepository $repAnnonce, CategoriesRepository $repCategorie, TypeLocationRepository $reptypeLocation, PaginationService $paginator)
     {
         $categories = $repCategorie->findAllWithSousCategorie();
-        $types      = $ReptypeLocation->findAllOrd();
+        $types      = $reptypeLocation->findAllOrd();
         $query      = $repAnnonce->findAllAnnonces();
         $annonces   = $paginator->paginate($query, $request->query->getInt('page', 1));
 
@@ -27,5 +31,28 @@ class AccueilController extends AbstractController
             'annonces' => $annonces,
             'types' => $types,
         ]);
+    }
+
+
+    /**
+     * @Route("/api/init", name="api_init")
+     */
+    public function categories(CategoriesRepository $repCategorie, SerializerInterface $serializer)
+    {
+        $categories = $repCategorie->findAllWithSousCategorie();
+        // dd( $categories );
+        $categories = $serializer->normalize($categories, null, [
+            AbstractObjectNormalizer::ENABLE_MAX_DEPTH => true,
+            AbstractNormalizer::IGNORED_ATTRIBUTES => [
+                'annonces', 
+                'className',
+                'id'
+            ]
+        ]);
+        
+        $datas = json_encode([
+            'categories' => $categories,
+        ]);
+        return new Response($datas, 200, ['Content-Type' => 'application/json']);
     }
 }
