e45f90f91e4b18d8654121e968ff268547e88f53
Henintsoa-coder <hentsraf@gmail.com>
2021-02-18 09:36:50 +0300

    Recherche des annonces : filtres (suite).

--- src/Repository/AnnoncesRepository.php
+++ src/Repository/AnnoncesRepository.php
@@ -220,13 +220,13 @@ class AnnoncesRepository extends ServiceEntityRepository
                 ->andWhere('l.dateDebut >= :date or l.dateFin <= :date or l.id is null')->setParameter('date', $criteria['date']);
         }
 
-        if (!empty($criteria['utilisateur_professionnel'])) {
+        if (!empty($criteria['utilisateur_professionnel']) && empty($criteria['utilisateur_particulier'])) {
             $query->innerJoin('a.user', 'er', 'WITH', 'er INSTANCE OF App\Entity\Professionnel');
         }
 
-        /*if (!empty($criteria['utilisateur_particulier'])) {
-            $query->innerJoin('a.user', 'er', 'WITH', 'er.discr = Particulier');
-        } */
+        if (!empty($criteria['utilisateur_particulier']) && empty($criteria['utilisateur_professionnel'])) {
+            $query->innerJoin('a.user', 'er', 'WITH', 'er INSTANCE OF App\Entity\Particulier');
+        }
 
         /*if (!empty($criteria['particulier'])) {
             $query->andWhere('a.urgent = 1');
