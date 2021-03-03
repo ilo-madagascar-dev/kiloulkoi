495027121d4928fdb49c34b733bf1b6bf9f452f8
Henintsoa-coder <hentsraf@gmail.com>
2021-03-03 12:57:54 +0300

    Retours après la réunion du 26 février.

--- src/Controller/Annonce/LocationController.php
+++ src/Controller/Annonce/LocationController.php
@@ -34,7 +34,7 @@ class LocationController extends AbstractController
     public function index(LocationRepository $locationRepository): Response
     {
         return $this->render('location/index.html.twig', [
-            'locations' => $locationRepository->findLocations( $this->getUser()->getId() ),
+            'locations' => $locationRepository->findLocations($this->getUser()->getId()),
         ]);
     }
 
@@ -54,8 +54,8 @@ class LocationController extends AbstractController
     public function enCours(LocationRepository $locationRepository): Response
     {
         return $this->render('location/enCours.html.twig', [
-            'mesBiens'    => $locationRepository->findMesBiens( $this->getUser()->getId() ),
-            'mesEmprunts' => $locationRepository->findMesEmprunts( $this->getUser()->getId() ),
+            'mesBiens'    => $locationRepository->findMesBiens($this->getUser()->getId()),
+            'mesEmprunts' => $locationRepository->findMesEmprunts($this->getUser()->getId()),
         ]);
     }
 
@@ -64,43 +64,37 @@ class LocationController extends AbstractController
      */
     public function new(Request $request, LocationRepository $locationRepository, AnnoncesRepository $annoncesRepository, StatutLocationRepository $statutReposistory, MangoPayService $mangoPayService, NotificationService $notificationService): Response
     {
-        
-        
+
+
         //vérify éligibility locataire
         $eligibilityBuyer = $mangoPayService->verifyKYCBANK($this->getUser()->getMangoPayId());
 
         //vérify éligibility propriétaire
-        $eligibilitySeller = $mangoPayService->verifyKYCBANK($annoncesRepository->find( $request->request->get('annonce'))->getUser()->getMangoPayId());
+        $eligibilitySeller = $mangoPayService->verifyKYCBANK($annoncesRepository->find($request->request->get('annonce'))->getUser()->getMangoPayId());
 
         //vérify amount in wallet user
         $amountWalletUser = $mangoPayService->getWallet($this->getUser()->getMangoPayId());
-        $prixLocation = intval($annoncesRepository->find( $request->request->get('annonce'))->getPrix()*100);
+        $prixLocation = intval($annoncesRepository->find($request->request->get('annonce'))->getPrix() * 100);
 
-        if ($eligibilitySeller == true) 
-        {
-            if( $eligibilityBuyer == true )
-            {
-                if ($amountWalletUser[0]->Balance->Amount >= $prixLocation)
-                {
+        if ($eligibilitySeller == true) {
+            if ($eligibilityBuyer == true) {
+                if ($amountWalletUser[0]->Balance->Amount >= $prixLocation) {
                     $token = $request->request->get('token');
-                    if ($this->isCsrfTokenValid('new_location', $token))
-                    {
+                    if ($this->isCsrfTokenValid('new_location', $token)) {
                         $user    = $this->getUser();
-                        $annonce = $annoncesRepository->find( $request->request->get('annonce'));
+                        $annonce = $annoncesRepository->find($request->request->get('annonce'));
                         $em      = $this->getDoctrine()->getManager();
 
                         $reservations = json_decode($request->request->get('reservations'));
                         $disponible   = $locationRepository->checkDates($reservations, $annonce->getId());
 
-                        if( $disponible && $annonce )
-                        {
+                        if ($disponible && $annonce) {
                             $statut = $statutReposistory->find(1); // Statut en attente
-                            foreach( $reservations as $reservation )
-                            {
+                            foreach ($reservations as $reservation) {
                                 $location = new Location();
 
-                                $location->setDateDebut( new DateTime($reservation->debut) );
-                                $location->setDateFin( new DateTime($reservation->fin) );
+                                $location->setDateDebut(new DateTime($reservation->debut));
+                                $location->setDateFin(new DateTime($reservation->fin));
                                 $location->setAnnonce($annonce);
                                 $location->setStatutLocation($statut);
                                 $location->setUser($user);
@@ -111,12 +105,12 @@ class LocationController extends AbstractController
                             $destinataire = $annonce->getUser();
 
                             $notification = new Notification();
-                            $photo        = $annonce->getPhoto()[0] ? '/uploads/'. $annonce->getPhoto()[0]->getUrl() : '/image/logo-fond-blanc.png';
-                            $notification->setDeclencheur( $user );
-                            $notification->setDestinataire( $destinataire );
-                            $notification->setMessage('Demande de location de l\'annonce <strong>'. $annonce->getTitre() .'</strong> par <strong>'. $user->getNomComplet() .'</strong>');
-                            $notification->setRoute( $this->generateUrl('location_en_cours') );
-                            $notification->setPhoto( $photo );
+                            $photo        = $annonce->getPhoto()[0] ? '/uploads/' . $annonce->getPhoto()[0]->getUrl() : '/image/logo-fond-blanc.png';
+                            $notification->setDeclencheur($user);
+                            $notification->setDestinataire($destinataire);
+                            $notification->setMessage('Demande de location de l\'annonce <strong>' . $annonce->getTitre() . '</strong> par <strong>' . $user->getNomComplet() . '</strong>');
+                            $notification->setRoute($this->generateUrl('location_en_cours'));
+                            $notification->setPhoto($photo);
 
                             $em->persist($notification);
                             $em->flush();
@@ -127,19 +121,15 @@ class LocationController extends AbstractController
                     }
 
                     return $this->redirectToRoute('location_en_cours');
-                }else{
+                } else {
                     $this->addFlash('notCards', 'Le Solde dans votre portefeuille est insuffisant pour cette location');
                     return $this->redirectToRoute('compte_portefeuille');
                 }
-            }
-            else
-            {
+            } else {
                 $this->addFlash('notCards', 'Vous n avez pas encore un moyen de paiement pour faire une location. ou vos documents ne sont pas encore valides');
                 return $this->redirectToRoute('compte_portefeuille');
             }
-        }
-        else
-        {
+        } else {
 
             $this->addFlash('warning', 'Le proprietaire n as pas encore de document valide, la location est annuler');
             return $this->redirectToRoute('location_en_cours');
@@ -147,7 +137,6 @@ class LocationController extends AbstractController
             /*$url = $this->generateUrl('user_profil');
             $this->addFlash('notCards', "Vous n'avez pas encore de moyen de paiement pour effectuer une location ou <a href=\"" . $url . "\">votre compte</a> n'a pas été vérifier.");
             return $this->redirectToRoute('compte_portefeuille');*/
-
         }
     }
 
@@ -161,18 +150,16 @@ class LocationController extends AbstractController
         $annonce   = $location->getAnnonce();
         $user      = $this->getUser();
 
-        if( $user->getId() === $annonce->getUser()->getId() )
-        {
-            if( $etat == "accepter" )
-            {
-                $periodeTotal = date_diff($location->getDateDebut(),$location->getDateFin());
+        if ($user->getId() === $annonce->getUser()->getId()) {
+            if ($etat == "accepter") {
+                $periodeTotal = date_diff($location->getDateDebut(), $location->getDateFin());
                 if ($annonce->getType()->getId() == 1) {
                     $difference = $periodeTotal->format('%h') + 1;
-                }elseif ($annonce->getType()->getId() == 2) {
+                } elseif ($annonce->getType()->getId() == 2) {
                     $difference = $periodeTotal->format('%a') + 1;
-                }elseif ($annonce->getType()->getId() == 3) {
-                    $difference = $periodeTotal->format('%a')/7 + 1;
-                }else{
+                } elseif ($annonce->getType()->getId() == 3) {
+                    $difference = $periodeTotal->format('%a') / 7 + 1;
+                } else {
                     $difference = $periodeTotal->format('%m') + 1;
                 }
 
@@ -181,18 +168,18 @@ class LocationController extends AbstractController
 
                 $destinataire = $locataire;
                 $notification = new Notification();
-                $photo        =  $annonce->getPhoto()[0] ? '/uploads/'. $annonce->getPhoto()[0]->getUrl() : '/image/logo-fond-blanc.png';
-                $notification->setDeclencheur( $user );
-                $notification->setDestinataire( $destinataire );
-                $notification->setMessage('Votre demande de location sur " <strong>'. $annonce->getTitre() .'</strong>" est acceptée!');
-                $notification->setRoute( $this->generateUrl('location_en_cours') );
-                $notification->setPhoto( $photo );
+                $photo        =  $annonce->getPhoto()[0] ? '/uploads/' . $annonce->getPhoto()[0]->getUrl() : '/image/logo-fond-blanc.png';
+                $notification->setDeclencheur($user);
+                $notification->setDestinataire($destinataire);
+                $notification->setMessage('Votre demande de location sur " <strong>' . $annonce->getTitre() . '</strong>" est acceptée!');
+                $notification->setRoute($this->generateUrl('location_en_cours'));
+                $notification->setPhoto($photo);
 
                 $this->getDoctrine()->getManager()->persist($notification);
 
                 //status
                 $statut    = $statutReposistory->find(2); // Statut en cours
-                $location->setStatutLocation( $statut );
+                $location->setStatutLocation($statut);
                 $this->getDoctrine()->getManager()->flush();
 
                 // send notification
@@ -203,19 +190,15 @@ class LocationController extends AbstractController
                     'proprio' => $user->getNomComplet(),
                     'locataire' => $locataire->getNomComplet()
                 ]);
-            }
-            else
-            {
+            } else {
                 $statut = $statutReposistory->find(4); // Statut interrompue
-                $location->setStatutLocation( $statut );
+                $location->setStatutLocation($statut);
                 $this->getDoctrine()->getManager()->persist($location);
                 $this->getDoctrine()->getManager()->flush();
             }
-        }
-        elseif( $locataire->getId() === $user->getId() && $etat == "refuser" )
-        {
+        } elseif ($locataire->getId() === $user->getId() && $etat == "refuser") {
             $statut    = $statutReposistory->find(4); // Statut interrompue
-            $location->setStatutLocation( $statut );
+            $location->setStatutLocation($statut);
             $this->getDoctrine()->getManager()->persist($location);
             $this->getDoctrine()->getManager()->flush();
         }
@@ -230,16 +213,15 @@ class LocationController extends AbstractController
     {
         $user = $this->getUser();
 
-        if( $user->getId() == $location->getUser()->getId() )
-        {
-            $valeur  = intval( $request->request->get('note') );
+        if ($user->getId() == $location->getUser()->getId()) {
+            $valeur  = intval($request->request->get('note'));
             $comment = $request->request->get('commentaire');
             $em      = $this->getDoctrine()->getManager();
 
             $note = new Note();
-            $note->setValeur( $valeur );
-            $note->setCommentaire( $comment );
-            $note->setLocation( $location );
+            $note->setValeur($valeur);
+            $note->setCommentaire($comment);
+            $note->setLocation($location);
 
             $em->persist($note);
             $em->flush();
