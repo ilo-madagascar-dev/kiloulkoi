bc052ba472e0db45f614278429d67bf949b1d1a2
Henintsoa-coder <hentsraf@gmail.com>
2021-03-05 09:27:27 +0300

    Affichage de la page de recherche.

--- public/js/accueil2.js
+++ public/js/accueil2.js
@@ -1,3 +1,4 @@
-$(".input-group-show-categorie").on("click", function () {
+$(".input-group-show-categorie").on("click", function (e) {
     $("#input-show-categorie").trigger('click');
+    //console.log(e.target);
 });
