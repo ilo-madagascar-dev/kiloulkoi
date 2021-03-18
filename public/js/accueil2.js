35151e35b25b717670d93cf40a1b568c0e7cfeef
Ilo Madagascar <henintsoa.rafidy@gmail.com>
2021-03-18 13:27:27 +0000

    js

--- public/js/accueil2.js
+++ public/js/accueil2.js
@@ -1,5 +1,3 @@
 $(".input-group-show-categorie").on("click", function (e) {
     $("#input-show-categorie").trigger('click');
-    //console.log(e.target);
-    //console.log($(e.target).hasClass('categorie-show-class'));
 });
