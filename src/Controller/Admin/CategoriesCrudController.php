d60932dca06c116a02be4fbdb2c408b961fa69e1
Ilo Madagascar <henintsoa.rafidy@gmail.com>
2021-03-24 11:38:56 +0000

    Modifications EasyAdmin

--- src/Controller/Admin/CategoriesCrudController.php
+++ src/Controller/Admin/CategoriesCrudController.php
@@ -3,6 +3,9 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Categories;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 
 class CategoriesCrudController extends AbstractCrudController
@@ -10,6 +13,15 @@ class CategoriesCrudController extends AbstractCrudController
     public static function getEntityFqcn(): string
     {
         return Categories::class;
+    }
+
+    public function configureActions(Actions $actions): Actions
+    {
+        return $actions
+            ->remove(Crud::PAGE_INDEX, Action::NEW)
+            ->remove(Crud::PAGE_INDEX, Action::EDIT)
+            ->remove(Crud::PAGE_INDEX, Action::DELETE)
+        ;
     }
 
     /*
