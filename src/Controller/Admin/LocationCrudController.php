299d3c5aea3a55de1825a2774cf6c8515ac94f58
Ilo Madagascar <henintsoa.rafidy@gmail.com>
2021-03-25 11:49:32 +0000

    Modifs admin.

--- src/Controller/Admin/LocationCrudController.php
+++ src/Controller/Admin/LocationCrudController.php
@@ -3,6 +3,9 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Location;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
@@ -24,7 +27,17 @@ class LocationCrudController extends AbstractCrudController
             AssociationField::new('user'),
             DateTimeField::new('dateDebut'),
             DateTimeField::new('dateFin'),
-            DateTimeField::new('dateReservation')
+            AssociationField::new('statutLocation'),
+            DateTimeField::new('dateReservation')->onlyOnDetail()
         ];
+    }
+
+    public function configureActions(Actions $actions): Actions
+    {
+        return $actions
+            ->add(Crud::PAGE_INDEX, Action::DETAIL)
+            ->remove(Crud::PAGE_INDEX, Action::NEW)
+            ->remove(Crud::PAGE_INDEX, Action::DELETE)
+            ->remove(Crud::PAGE_INDEX, Action::EDIT);
     }
 }
--- src/Entity/StatutLocation.php
+++ src/Entity/StatutLocation.php
@@ -98,4 +98,9 @@ class StatutLocation
 
         return $this;
     }
+
+    public function __toString()
+    {
+        return $this->libelle; 
+    }
 }
