fbbf003cb7dcf79ab0a7ae70616a7ba8747f5531
Ilo Madagascar <henintsoa.rafidy@gmail.com>
2021-04-29 10:26:11 +0000

    Partie Admin : NotesCrud.

--- src/Controller/Admin/NoteCrudController.php
+++ src/Controller/Admin/NoteCrudController.php
@@ -3,18 +3,28 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Note;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\NumberField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
-use EasyCorp\Bundle\EasyAdminBundle\Field\NumberField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
 class NoteCrudController extends AbstractCrudController
 {
     public static function getEntityFqcn(): string
     {
         return Note::class;
+    }
+
+    public function configureActions(Actions $actions): Actions
+    {
+        return $actions
+            ->remove(Crud::PAGE_INDEX, Action::NEW)
+            ->remove(Crud::PAGE_INDEX, Action::EDIT);
     }
 
     public function configureFields(string $pageName): iterable
